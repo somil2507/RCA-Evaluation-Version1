ade750f05fb2107e556d593fed4267ae19ac46e3
Somil Mehta <somil250497@gmail.com>
2025-07-21 12:27:06 +0530

    removed commented lines from routing.py

--- routing.py
+++ routing.py
@@ -1,11 +1,3 @@
-# def assign_team(root_cause, component, matrix):
-#     try:
-#         if component and component in matrix.get(root_cause, {}):
-#             return matrix[root_cause][component]
-#         return matrix[root_cause].get("default", "General Support Team")
-#     except:
-#         return "Unmapped - Needs Manual Review"
-#
 def assign_team(root_cause, matrix):
     try:
         return matrix.get(root_cause, {}).get("default", "General Support Team")
